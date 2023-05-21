82efb04bfb90282ff1da2a2d65d4e5c5a56a6eeb
Mark Bundschuh <mark@mbund.org>
2023-05-21 19:01:25 -0400

    remove cloudflare tunnel from terraform

--- terraform/modules/cloudflare/main.tf
+++ terraform/modules/cloudflare/main.tf
@@ -19,43 +19,6 @@ locals {
   ]
 }
 
-resource "random_password" "tunnel_secret" {
-  length  = 64
-  special = false
-}
-
-resource "cloudflare_tunnel" "homelab" {
-  account_id = var.cloudflare_account_id
-  name       = "homelab"
-  secret     = base64encode(random_password.tunnel_secret.result)
-}
-
-# Not proxied, not accessible. Just a record for auto-created CNAMEs by external-dns.
-resource "cloudflare_record" "tunnel" {
-  zone_id = data.cloudflare_zone.zone.id
-  type    = "CNAME"
-  name    = "homelab-tunnel"
-  value   = "${cloudflare_tunnel.homelab.id}.cfargotunnel.com"
-  proxied = false
-  ttl     = 1 # Auto
-}
-
-resource "kubernetes_secret" "cloudflared_credentials" {
-  metadata {
-    name      = "cloudflared-credentials"
-    namespace = "cloudflared"
-  }
-
-  data = {
-    "credentials.json" = jsonencode({
-      AccountTag   = var.cloudflare_account_id
-      TunnelName   = cloudflare_tunnel.homelab.name
-      TunnelID     = cloudflare_tunnel.homelab.id
-      TunnelSecret = base64encode(random_password.tunnel_secret.result)
-    })
-  }
-}
-
 resource "cloudflare_api_token" "external_dns" {
   name = "homelab_external_dns"
 
