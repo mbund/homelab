ec7b35916ce7ea6997600c740e514199f5179b55
Mark Bundschuh <mark@mbund.org>
2023-06-02 10:28:38 -0400

    dont create service account pihole

--- terraform/modules/tailscale/main.tf
+++ terraform/modules/tailscale/main.tf
@@ -8,3 +8,14 @@ resource "kubernetes_secret" "tailscale_auth" {
     TS_AUTHKEY = var.tailscale_auth_key
   }
 }
+
+resource "kubernetes_secret" "tailscale_auth_pihole" {
+  metadata {
+    name      = "tailscale-auth"
+    namespace = "pihole"
+  }
+
+  data = {
+    TS_AUTHKEY = var.tailscale_auth_key
+  }
+}
