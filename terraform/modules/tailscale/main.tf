921e57c19511bc0b58667795b7cff570ac241871
Mark Bundschuh <mark@mbund.org>
2023-06-18 13:34:15 -0400

    terraform remove pihole tailscale auth secret

--- terraform/modules/tailscale/main.tf
+++ terraform/modules/tailscale/main.tf
@@ -8,14 +8,3 @@ resource "kubernetes_secret" "tailscale_auth" {
     TS_AUTHKEY = var.tailscale_auth_key
   }
 }
-
-resource "kubernetes_secret" "tailscale_auth_pihole" {
-  metadata {
-    name      = "tailscale-auth"
-    namespace = "pihole"
-  }
-
-  data = {
-    TS_AUTHKEY = var.tailscale_auth_key
-  }
-}
